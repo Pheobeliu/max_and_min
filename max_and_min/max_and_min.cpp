d48f90a7104591cc614af0af54ebef479127871b
Pheobeljj <liujj0825@163.com>
2018-06-02 18:25:13 +0800

    最新

--- max_and_min/max_and_min.cpp
+++ max_and_min/max_and_min.cpp
@@ -6,6 +6,28 @@
 //求最大公约数和最小公倍数
 int main()
 {
+	int m, n, a, b,k;
+	
+	printf("请输入两个整数，用逗号隔开：");
+	
+	scanf_s("%d,%d", &m, &n);
+	a = m;
+	b = n;
+	if (a < b)
+	{
+		k = a;
+		a = b;
+		b = k;
+	}
+	while ((k = a%b) != 0) {
+		a = b;
+		b = k;
+	}
+	printf("最大公约数为：%d\n", b);
+	printf("最小公倍数为：%d\n", m*n/b );
+	getchar();
+	
+	getchar();
     return 0;
 }
 
